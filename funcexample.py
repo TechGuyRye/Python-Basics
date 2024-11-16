c69dd8e7e62cbca01c4c093c074d59515cc482a5
Ryan <techguyrye@gmail.com>
2024-11-16 14:26:42 -0600

    Edited function file

--- funcexample.py
+++ funcexample.py
@@ -18,5 +18,16 @@ def yourfunction(somevariable):
 definedvariable = ("Goodbye World!")
 yourfunction(definedvariable)
 
-#What we've done here is 1. defined yourfunction(), 2. called yourfunction(), 3. passed the variable definedvariable. 4. This value being passed to a function is an argument; if the argument is assigned to a local variable within the function, this is known as a parameter
-    
+#What we've done here is 1. defined yourfunction(), 2. called yourfunction(), 3. passed the variable definedvariable. 4. This value being passed to a function is an argument; if the argument is assigned to a local variable within the function, this is known as a parameter 
+
+#------------------------------------------------------------------------
+
+#We also have the ability to return values in a function, so that they can be used elsewhere in the program. For example:
+
+def somefunction():
+    answer = 5 + 5
+    return answer
+
+print (somefunction())
+
+#Here we 1. defined a function (somefunction) that returns the 'answer' variable to where the function was called, and 3. we print the returned value after the function is called. 
